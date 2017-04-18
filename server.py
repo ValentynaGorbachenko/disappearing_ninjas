344fd05a2527c2d8f3e3d207649033c3f45e0d37
ValentynaGorbachenko <gorbachenko.valentyna@gmail.com>
2017-04-18 00:03:28 -0700

    Update routes in server.py, script.js for ajax call, add partial

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, render_template, redirect 
+from flask import Flask, request, render_template, redirect, render_template_string
 app = Flask(__name__)
 app.secret_key = "\xe8\xd5\x9e\x1f\x9da\x94\xe5\x88\\%\xde\xa6\xab\xca\x81\x1a\xd8\x0e\xacl\x94U\xd1"
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/ninjas')
+def all_ninjas():
+	print "inside the ninjas"
+	return render_template('partials/_all_ninjas.html')
 
 app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+$(document).ready(function(){
+	$('body').on('click', 'a', function(){
+		event.preventDefault();
+		console.log($(this).attr("href"));
+		$.get($(this).attr('href'), function(response){
+			console.log("Hey !!!!")	
+			console.log(typeof(response));
+			$('#initial').css('display', 'none');
+			$('a').css('display', 'inline-block');
+			$("#result").html(response);
+		});
+	})
+});
